1d933ba76877e627bbcfbcb65ae6294750e22008
relrelb <relrelbachar@gmail.com>
2021-02-03 14:41:02 +0200

    Cleanup

--- core/src/display_object/container.rs
+++ core/src/display_object/container.rs
@@ -324,8 +324,7 @@ macro_rules! impl_display_object_container {
             drop(write);
 
             if let Some(removed_child) = removed_child {
-                context
-                    .remove_node(removed_child);
+                context.remove_node(removed_child);
             }
             context.add_node(child);
 
@@ -559,7 +558,7 @@ impl<'gc> ChildContainer<'gc> {
     /// unset the parent either as that's expected to happen after unloading.
     pub fn remove_child_from_exec_list(
         &mut self,
-        context: &UpdateContext<'_, 'gc, '_>,
+        context: &mut UpdateContext<'_, 'gc, '_>,
         child: DisplayObject<'gc>,
     ) -> bool {
         // Remove from children linked list.
