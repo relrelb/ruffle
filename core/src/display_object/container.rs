764c9c1f13df3677b2be0f592ddde6c909847f30
relrelb <relrelbachar@gmail.com>
2021-02-05 15:44:35 +0200

    More cleanup

--- core/src/display_object/container.rs
+++ core/src/display_object/container.rs
@@ -299,9 +299,9 @@ macro_rules! impl_display_object_container {
             drop(write);
 
             if let Some(removed_child) = removed_child {
-                context.remove_node(removed_child);
+                context.remove_from_execution_list(removed_child);
             }
-            context.add_node(child);
+            context.add_to_execution_list(child);
 
             child.set_parent(context.gc_context, Some(self.into()));
             child.set_level(context.gc_context, self.level());
@@ -394,7 +394,7 @@ macro_rules! impl_display_object_container {
             drop(write);
 
             let removed_from_execution_list =
-                from_lists.contains(Lists::EXECUTION) && context.remove_node(child);
+                from_lists.contains(Lists::EXECUTION) && context.remove_from_execution_list(child);
 
             if removed_from_execution_list {
                 child.unload(context);
@@ -422,7 +422,7 @@ macro_rules! impl_display_object_container {
                 write.$field.remove_child_from_depth_list(removed);
                 drop(write);
 
-                context.remove_node(removed);
+                context.remove_from_execution_list(removed);
 
                 removed.unload(context);
 
@@ -641,7 +641,7 @@ impl<'gc> ChildContainer<'gc> {
             }
         } else {
             self.render_list.insert(id, child);
-            context.add_node(child);
+            context.add_to_execution_list(child);
         }
     }
 
